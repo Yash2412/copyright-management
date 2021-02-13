c4680498e0fa5d98fa85802684eaaeafda8953a3
hraj6056 <hraj1187@gmail.com>
2021-02-13 15:48:30 +0530

    Adding solidity contracts

--- contracts/NewBlock.sol
+++ contracts/NewBlock.sol
@@ -1,5 +1,24 @@
-pragma solidity ^0.7.4;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.0;
 
 contract NewBlock {
+    uint public imageCount = 0;
 
+    struct ImageInfo{
+        uint imageID;
+        string pHash;
+        string ipfsAddress;
+        string owner_name;
+        string owner_email;
+        string image_title;
+    }
+
+    mapping(uint => ImageInfo) public images;
+
+    function addImage(string memory _pHash, string memory _ipfsAddress, string memory _owner_name, string memory _owner_email, string memory _image_title) public{
+        uint result = uint(sha256(abi.encodePacked(_owner_email)));
+
+        imageCount++;
+        images[imageCount] = ImageInfo(result, _pHash, _ipfsAddress, _owner_name, _owner_email, _image_title);
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const NewBlock = artifacts.require("./NewBlock.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(NewBlock);
 };
